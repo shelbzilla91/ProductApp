fe06788871a95768c09589221b7d9a5da2a30ad8
Shelby Baker <shelbymbaker91@gmail.com>
2019-06-28 11:41:43 -0500

    stuck

--- productApp/app/assets/javascripts/components/Products.js.jsx
+++ productApp/app/assets/javascripts/components/Products.js.jsx
@@ -1,11 +1,8 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
 
-console.log('User schedule file test')
 
 class Products extends React.Component {
   render() {
-    console.log('User schedule inside render')
+    console.log('Product is here')
     return (
       <div className="container-fluid">
         <p>TEST THAT THIS WORKS</p>
